9b861dad12d69525a974c13200cf24a5e8d42edd
bhanuk <kbhanuchandra1@gmail.com>
2013-12-12 11:27:10 +0530

    Update test.java

--- first/src/test.java
+++ first/src/test.java
@@ -1,12 +1,19 @@
-
 public class test {
 
 	/**
 	 * @param args
 	 */
+	 class test{
+	 	public void act(){
+	 		System.out.println("Hello GitHub inside Method");
+	 	}
+	 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub");
+		//call innerclass method
+		test t=new test();
+		t.act();
 
 	}
 
